4908b564af0b2654e3d025b5c5641a384d84cc5c
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-31 17:44:59 +0530

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import React from 'react';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24" />
   );
 }
