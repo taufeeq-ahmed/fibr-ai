0f522978b2ce236fc902d5b861fea4b930653217
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-05-31 21:31:48 +0530

    supabase integrated

--- src/app/create-page/page.tsx
+++ src/app/create-page/page.tsx
@@ -6,11 +6,26 @@ import { useForm, SubmitHandler, UseFormReturn } from 'react-hook-form';
 import { CreatePageInputs } from '@/types/create-page';
 import ViewPage from '@/page-components/create-page/view-page';
 import CreateForm from '@/page-components/create-page/create-form';
+import supabase from '@/db/supabase';
+import { toKebabCase } from '@/lib/utils';
 
 function CreatePage() {
   const formControls:UseFormReturn<CreatePageInputs> = useForm<CreatePageInputs>();
 
-  const onSubmit: SubmitHandler<CreatePageInputs> = (data) => console.log(data);
+  const onSubmit: SubmitHandler<CreatePageInputs> = async (data) => {
+    const { title, description } = data;
+
+    await supabase
+      .from('pages')
+      .upsert({
+        title,
+        description,
+        slug: toKebabCase(title),
+      })
+      .select();
+
+    console.log('saved');
+  };
 
   return (
     <div className="flex">
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,17 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable import/prefer-default-export */
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
-export function cn(...inputs: ClassValue[]) {
+function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+function toKebabCase(text: string): string {
+  text = text.toLowerCase();
+  text = text.replace(/[^a-z0-9]+/g, '-');
+  text = text.replace(/^-+|-+$/g, '');
+  return text;
+}
+
+export { cn, toKebabCase };
