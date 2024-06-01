adef68ccc1a046ed6cd40d87002c06fa06afd098
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-06-01 11:51:53 +0530

    buttons edited

--- src/page-components/dashboard-page/ListPagesItem/index.tsx
+++ src/page-components/dashboard-page/ListPagesItem/index.tsx
@@ -1,4 +1,4 @@
-import { MdOutlinePreview } from 'react-icons/md';
+import { MdOutlinePreview, MdDeleteForever } from 'react-icons/md';
 import { FaEdit } from 'react-icons/fa';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
@@ -16,17 +16,16 @@ function ListPagesItem({
   title, description, isLive, slug,
 }:ListPagesItemProps) {
   return (
-    <div className="border-2 w-1/2 rounded-md p-4 flex justify-between">
+    <div className="border-2 rounded-md p-4 flex justify-between h-[200px] overflow-hidden">
 
       <div className="w-6/12 flex flex-col justify-start">
-        <h3 className="font-semibold mb-2">{title}</h3>
+        <h3 className="font-semibold mb-2 text-lg font-bold">{title}</h3>
         <p className="text-ellipsis">
           {truncateText(description, 300)}
         </p>
-        <p>{isLive}</p>
       </div>
 
-      <div className="w-3/12 flex flex-col gap-2">
+      <div className="w-3/12 flex flex-col gap-2 items-end">
 
         <Link href={`/preview-page/${slug}`}>
           <Button className="font-lg flex gap-2">
@@ -38,13 +37,26 @@ function ListPagesItem({
         </Link>
 
         <Link href={`/edit-page/${slug}`}>
-          <Button className="font-lg flex gap-2">
+          <Button
+            className="font-lg flex gap-2 text-blue-500 border-blue-500"
+            variant="outline"
+          >
             {' '}
             <FaEdit size={15} />
             Edit
             {' '}
           </Button>
         </Link>
+
+        <Button
+          className="font-lg flex gap-2 bg-white text-red-500 border-red-500"
+          variant="outline"
+        >
+          {' '}
+          <MdDeleteForever size={20} />
+          Delete
+          {' '}
+        </Button>
 
       </div>
 
