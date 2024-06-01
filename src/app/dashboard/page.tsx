d80c95891b1e1ac98423c19e9966cfa6df94b452
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-06-01 10:40:20 +0530

    edit page is added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,10 @@
+import { Button } from '@/components/ui/button';
 import supabase from '@/db/supabase';
 import PreviewPage from '@/page-components/create-page/preview-page';
+import Link from 'next/link';
 import React from 'react';
+import { MdOutlinePreview } from 'react-icons/md';
+import { FaEdit } from 'react-icons/fa';
 
 const getAllLandingPages = async () => {
   const { data: pages } = await supabase
@@ -26,15 +30,32 @@ function ListPagesItem({
   title, description, isLive, slug,
 }:ListPagesItemProps) {
   return (
-    <div className="border-2 rounded-md p-4 h-[200px] flex justify-start">
+    <div className="border-2 rounded-md p-4 h-[200px] flex justify-between">
 
       <PreviewPage title={title} description={description} />
 
-      <div>
+      <div className="flex flex-col justify-start flex-1">
         <h3 className="font-semibold mb-2">{title}</h3>
         <p className="text-ellipsis">{truncateText(description, 300)}</p>
         <p>{isLive}</p>
-        <p>{slug}</p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <Link href={`/preview-page/${slug}`}>
+          <Button className="font-lg flex gap-2">
+            {' '}
+            <MdOutlinePreview size={20} />
+            Preview
+            {' '}
+          </Button>
+        </Link>
+        <Link href={`/edit-page/${slug}`}>
+          <Button className="font-lg flex gap-2">
+            {' '}
+            <FaEdit size={15} />
+            Edit
+            {' '}
+          </Button>
+        </Link>
       </div>
     </div>
   );
---
+++ src/app/preview-page/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PreviewPage() {
+  return (
+    <div>PreviewPage</div>
+  );
+}
+
+export default PreviewPage;
