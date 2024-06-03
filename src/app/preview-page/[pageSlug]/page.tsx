aa682fcc4812f3f1e316a240bbdf3259a2d94396
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-06-03 11:15:30 +0530

    preview page updated successfully

--- src/app/preview-page/[pageSlug]/page.tsx
+++ src/app/preview-page/[pageSlug]/page.tsx
@@ -3,6 +3,9 @@
 import React, { useEffect, useState } from 'react';
 import supabase from '@/db/supabase';
 import MobileView from '@/components/ui/mobileview';
+import { Button } from '@/components/ui/button';
+import { CiMedicalClipboard } from 'react-icons/ci';
+import { toast } from 'sonner';
 
 const getPage = async (slug:string) => {
   const { data: pages } = await supabase
@@ -44,13 +47,30 @@ function PreviewPage({ params }: { params: { pageSlug: string } }) {
     title, description, imageUrl, buyLink,
   } = page;
 
+  const handleCopy = () => {
+    const publicUrl = window.location.href;
+    navigator.clipboard.writeText(publicUrl);
+
+    toast.success('Url copied to clipboard successfully');
+  };
+
   return (
-    <MobileView
-      title={title}
-      description={description}
-      image={imageUrl}
-      buyLink={buyLink}
-    />
+    <section className="flex justify-between items-center gap-4 w-[50%] m-auto">
+      <MobileView
+        title={title}
+        description={description}
+        image={imageUrl}
+        buyLink={buyLink}
+      />
+      <Button
+        variant="outline"
+        className="flex gap-2"
+        onClick={handleCopy}
+      >
+        <CiMedicalClipboard size={20} />
+        Click to copy Public URL
+      </Button>
+    </section>
   );
 }
 
