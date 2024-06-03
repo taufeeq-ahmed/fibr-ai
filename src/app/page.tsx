086ee9eca7657040afc2b9d1a2637af87a00c45a
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-06-03 10:56:49 +0530

    navbar styles updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 
 function Home() {
   return (
-    <h1>Hello</h1>
+    <section className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl text-center font-semibold">
+        The
+        {' '}
+        <span className="text-blue-500 ">AI-native marketing platform</span>
+        {' '}
+        <br />
+        for enterprise success
+      </h1>
+    </section>
   );
 }
 
